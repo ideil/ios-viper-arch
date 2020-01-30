8a3592e149887ebc307e7ff2a9e7c330446af616
Eduard Pelesh <edpelesh@gmail.com>
2020-01-31 00:11:47 +0200

    fix package manifest

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "ViperArch",
     platforms: [
-        .iOS(.v10),
-        .tvOS(.v10),
+        .iOS(.v11),
+        .tvOS(.v11),
     ],
     products: [
         .library(name: "ViperArch", targets: ["ViperArch"]),
-        .library(name: "ViperArch", targets: ["ViperArchTV"]),
     ],
     targets: [
-        .target(name: "ViperArch", dependencies: [], path: ".", exclude: ["ViperArchTV"]),
-        .target(name: "ViperArchTV", dependencies: [], path: ".", exclude: ["ViperArch"]),
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(name: "ViperArch", dependencies: [], path: "Source"),
+    ]
 )
