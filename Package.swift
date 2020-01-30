78cb253d8b1096528a116a875eab979853199d6a
Eduard Pelesh <edpelesh@gmail.com>
2020-01-30 23:57:46 +0200

    swift package created

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "ViperArch",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10),
+    ],
+    products: [
+        .library(name: "ViperArch", targets: ["ViperArch"]),
+        .library(name: "ViperArch", targets: ["ViperArchTV"]),
+    ],
+    targets: [
+        .target(name: "ViperArch", dependencies: [], path: ".", exclude: "ViperArchTV"),
+        .target(name: "ViperArchTV", dependencies: [], path: ".", exclude: "ViperArch"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
